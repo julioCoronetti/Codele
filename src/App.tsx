8949aa615f55c06527fb8b0533ce1d73837a098f
julioCoronetti <timeset13@outlook.com>
2025-06-24 11:51:49 -0300

    feat: enhance App component layout with new styling and add LightbulbIcon; update Row component styles for improved visual feedback

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { type ChangeEvent, useEffect, useId, useRef, useState } from "react";
 import { Row } from "./components/Row";
 import { Dialog, DialogContent } from "./components/ui/dialog";
 import { getTodayTerm } from "./utils/getTodayWord";
+import { LightbulbIcon } from "lucide-react";
 
 const term = getTodayTerm();
 
@@ -39,61 +40,73 @@ const App = () => {
 	const id = useId();
 
 	return (
-		<div className="h-screen flex flex-col items-center space-y-5 ">
-			<h1 className="text-6xl text-gray-700">Codele</h1>
-
-			<div className="flex flex-col">
-				{Array.from({ length: maxAttempts }).map((_, index) => (
-					<Row
-						key={term.word}
-						guess={guesses[index] || ""}
-						targetWord={targetWord}
-					/>
-				))}
-			</div>
-
-			{!isGameOver && (
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						handleGuess();
-					}}
-					className="flex items-center gap-5"
+		<main className="h-screen w-screen flex flex-col bg-gradient-to-tl from-gray-500 to-gray-300">
+			<nav className="w-full h-15 border-b-1 border-white bg-gray-700 flex items-center justify-between px-5 tracking-widest">
+				<p className="text-2xl font-bold text-white">Codele</p>
+				<button
+					type="button"
+					className="w-15 h-full flex justify-center items-center cursor-pointer hover:bg-gray-500 transition duration-150 ease-in-out"
 				>
-					<input
-						className="border-5 border-gray-700 outline-0 rounded-xl px-5 py-3 text-xl font-bold"
-						ref={inputRef}
-						onChange={handleInputChange}
-						maxLength={targetWord.length}
-						placeholder="Enter your guess"
-						value={currentGuess}
-						id={id}
-					/>
-					<button
-						className="bg-gray-700 px-5 py-3 text-xl font-bold text-white outline-0 shadow-xl shadow-black/30 cursor-pointer rounded-xl transition duration-150 ease-in-out hover:scale-110"
-						type="submit"
+					<LightbulbIcon className="text-white" />
+				</button>
+			</nav>
+			<h1 className="text-[10rem] tracking-[3rem] text-gray-700/15 absolute z-0 select-none left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-bold">
+				Codele
+			</h1>
+			<div className="h-full flex flex-col items-center justify-center relative gap-5">
+				<div className="flex flex-col z-10">
+					{Array.from({ length: maxAttempts }).map((_, index) => (
+						<Row
+							key={term.word}
+							guess={guesses[index] || ""}
+							targetWord={targetWord}
+						/>
+					))}
+				</div>
+
+				{!isGameOver && (
+					<form
+						onSubmit={(e) => {
+							e.preventDefault();
+							handleGuess();
+						}}
+						className="flex items-center gap-5"
 					>
-						ENTER
-					</button>
-				</form>
-			)}
-			{isGameOver && guesses[guesses.length - 1] === targetWord && (
-				<Dialog open={isGameOver} onOpenChange={setIsGameOver}>
-					<DialogContent className="h-50 w-90 flex flex-col items-center justify-center text-gray-700 bg-white/90">
-						<h1 className="text-4xl text-green-500">Congratulations!</h1>
-						<p className="text-2xl">{`The correct word is ${targetWord}!`}</p>
-					</DialogContent>
-				</Dialog>
-			)}
-			{isGameOver && guesses[guesses.length - 1] !== targetWord && (
-				<Dialog open={isGameOver} onOpenChange={setIsGameOver}>
-					<DialogContent className="h-50 w-90 flex flex-col items-center justify-center text-gray-700 bg-white/90">
-						<h1 className="text-4xl text-red-500">Game over!</h1>
-						<p className="text-2xl">{`The word was: ${targetWord}`}</p>
-					</DialogContent>
-				</Dialog>
-			)}
-		</div>
+						<input
+							className="border-5 border-gray-700 outline-0 rounded-sm px-5 py-3 text-xl font-bold"
+							ref={inputRef}
+							onChange={handleInputChange}
+							maxLength={targetWord.length}
+							placeholder="Enter your guess"
+							value={currentGuess}
+							id={id}
+						/>
+						<button
+							className="bg-gray-700 px-5 py-3 text-xl font-bold text-white outline-0 shadow-xl shadow-black/30 cursor-pointer rounded-sm transition duration-150 ease-in-out hover:scale-110"
+							type="submit"
+						>
+							ENTER
+						</button>
+					</form>
+				)}
+				{isGameOver && guesses[guesses.length - 1] === targetWord && (
+					<Dialog open={isGameOver} onOpenChange={setIsGameOver}>
+						<DialogContent className="h-50 w-90 flex flex-col items-center justify-center text-gray-700 bg-white/90">
+							<h1 className="text-4xl text-green-500">Congratulations!</h1>
+							<p className="text-2xl">{`The correct word is ${targetWord}!`}</p>
+						</DialogContent>
+					</Dialog>
+				)}
+				{isGameOver && guesses[guesses.length - 1] !== targetWord && (
+					<Dialog open={isGameOver} onOpenChange={setIsGameOver}>
+						<DialogContent className="h-50 w-90 flex flex-col items-center justify-center text-gray-700 bg-white/90">
+							<h1 className="text-4xl text-red-500">Game over!</h1>
+							<p className="text-2xl">{`The word was: ${targetWord}`}</p>
+						</DialogContent>
+					</Dialog>
+				)}
+			</div>
+		</main>
 	);
 };
 
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -17,14 +17,14 @@ export const Row = ({ guess, targetWord }: RowProps) => {
 	const letters = guess.padEnd(targetWord.length).split("");
 
 	return (
-		<div className="flex justify-center m-[5px]">
+		<div className="flex justify-center ">
 			{letters.map((letter, index) => {
 				const isFilled = letter.trim().length > 0;
 				return (
 					<span
 						key={letter}
-						className={`w-13 h-13 inline-flex justify-center items-center mx-0.5 my-1 font-bold text-3xl rounded
-                            ${isFilled ? "text-white" : "text-transparent border-3 border-gray-700 bg-transparent rounded-xl"}
+						className={`w-15 h-15 inline-flex justify-center items-center mx-0.5 my-1 font-bold text-3xl rounded
+                            ${isFilled ? "text-white" : "text-transparent border-3 border-gray-700 bg-transparent rounded-sm shadow-md shadow-black/30 select-none"}
                             ${isFilled && getLetterStatus(letter, index) === "correct" ? "bg-green-500" : ""}
                             ${isFilled && getLetterStatus(letter, index) === "present" ? "bg-yellow-500" : ""}
                             ${isFilled && getLetterStatus(letter, index) === "absent" ? "bg-red-500" : ""}
