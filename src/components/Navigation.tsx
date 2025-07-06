50e28b52ab6897fbebeee2e77fecfabdf088ac54
julioCoronetti <timeset13@outlook.com>
2025-07-06 20:45:42 -0300

    fix: add missing import for ModeToggle component in Navigation.tsx fix: add missing newline at end of file in progress.ts

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,7 @@
 import { ChartNoAxesColumn, LightbulbIcon } from "lucide-react";
 import { getTodayTerm } from "@/utils/getTodayWord";
 import type { Progress } from "@/utils/progress";
+import { ModeToggle } from "./ModeToggle";
 import {
 	Dialog,
 	DialogContent,
@@ -8,7 +9,6 @@ import {
 	DialogTitle,
 	DialogTrigger,
 } from "./ui/dialog";
-import { ModeToggle } from "./ModeToggle";
 
 type NavigationProps = {
 	guesses: string[];
