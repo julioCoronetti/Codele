117e0b87452345cafcd62eac11c67f73948524e1
Julio Coronetti <timeset13@outlook.com>
2025-07-08 14:45:30 +0000

    feat: adicionar descrição ao componente ProgressDialog e ajustar o tamanho do título

--- src/components/ProgressDialog.tsx
+++ src/components/ProgressDialog.tsx
@@ -1,5 +1,5 @@
 import type { Progress } from "@/utils/progress";
-import { DialogContent, DialogTitle } from "./ui/dialog";
+import { DialogContent, DialogDescription, DialogTitle } from "./ui/dialog";
 
 type ProgressDialogProps = {
 	progress: Progress;
@@ -8,7 +8,10 @@ type ProgressDialogProps = {
 export const ProgressDialog = ({ progress }: ProgressDialogProps) => {
 	return (
 		<DialogContent className="h-150 w-200 flex flex-col items-center p-20 text-white bg-gray-700 rounded-sm">
-			<DialogTitle className="text-4xl text-white">Progress</DialogTitle>
+			<DialogTitle className="text-5xl text-white">Progress</DialogTitle>
+			<DialogDescription className="text-md text-white/70">
+				See your stats!
+			</DialogDescription>
 			<div className="flex flex-row justify-center items-end gap-20 mt-10">
 				<div className="flex flex-col items-center">
 					<span className="text-5xl font-bold">{progress.games}</span>
