548e200df79a1edfb70c9cd0964f5fe149cf1326
Julio Coronetti <timeset13@outlook.com>
2025-07-08 14:02:52 +0000

    feat: adicionar ProgressProvider ao componente principal para gerenciamento de progresso

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./components/ThemeProvider.tsx";
 import { TermProvider } from "./contexts/TermContext.tsx";
+import { ProgressProvider } from "./contexts/ProgressContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 			<TermProvider>
-				<App />
+				<ProgressProvider>
+					<App />
+				</ProgressProvider>
 			</TermProvider>
 		</ThemeProvider>
 	</StrictMode>,
